b8cc02d637eb63cf57f3b3ed05978175c1c16906
zukas97 <zukaslurker97@proton.me>
2025-04-17 10:53:44 -0400

    add basic stat struct

--- src/character.h
+++ src/character.h
@@ -1,13 +1,22 @@
 #include <raylib.h>
 
 typedef struct {
+    int health;
+    int resistance;
+    int attack;
+    int faith;
+} Stats;
+
+typedef struct {
     Texture2D texture;
     char* name;
     Rectangle frameRec;
     int x, y;
     int w, h;
+    Stats stats;
 } Character;
 
+int check_stat(char* stat);
 
 Character create_character(Image image, int x, int y, int w, int h, char* name);
 
